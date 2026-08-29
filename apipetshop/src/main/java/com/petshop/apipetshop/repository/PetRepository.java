cf02480109411986d24536d855f2ac78cc61fbcb
SamucaWU <samucaWhatsUp@gmail.com>
2026-08-29 01:31:31 -0300

    feat: PetRepository atualizada com pesquisas customizadas e PetService em andamento de configurações.

--- apipetshop/src/main/java/com/petshop/apipetshop/repository/PetRepository.java
+++ apipetshop/src/main/java/com/petshop/apipetshop/repository/PetRepository.java
@@ -1,10 +1,15 @@
 package com.petshop.apipetshop.repository;
 
-import com.petshop.apipetshop.model.Pet;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.petshop.apipetshop.model.Pet;
+
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
-    
+    List<Pet> findByContaining(String nome);// buscar por nome do pet
+    List<Pet> findByTutorId(Long tutorId); // buscar por tutorId
+    List<Pet> findByEspecie(String especie); // buscar por especie do pet
 }
--- apipetshop/src/main/java/com/petshop/apipetshop/service/PetService.java
+++ apipetshop/src/main/java/com/petshop/apipetshop/service/PetService.java
@@ -1,7 +1,12 @@
 package com.petshop.apipetshop.service;
 
+import org.springframework.stereotype.Service;
+
+import com.petshop.apipetshop.DTO.petDTO.petRequestDTO;
+import com.petshop.apipetshop.DTO.petDTO.petResponseDTO;
 import com.petshop.apipetshop.repository.PetRepository;
 
+@Service
 public class PetService {
     
     private final PetRepository petRepository;
@@ -11,6 +16,9 @@ public class PetService {
     }
     
     //cadastrar
+    public petResponseDTO cadastrarPet(petRequestDTO dto) {
+        if()
+    }
     //buscar
     //buscarId
     //atualizar
